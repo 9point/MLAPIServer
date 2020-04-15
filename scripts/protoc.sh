ea7d39034a4008a30134d4de54a1b9efa8150e3d
Brendan McNamara <me@brendan9.com>
2020-04-15 12:25:34 -0700

    Cleans up protoc script

--- scripts/protoc.sh
+++ scripts/protoc.sh
@@ -1,17 +1,24 @@
 #!/bin/bash
 
-mkdir -p ./src/static_codegen
-
 PROTO_DIR=./proto/
-CODEGEN_DIR=../src/static_codegen
+CODEGEN_DIR=./src/static_codegen
+
+rm -rf ${CODEGEN_DIR}
+mkdir -p ${CODEGEN_DIR}
+
+WD=$(pwd)
 
 cd ${PROTO_DIR}
+
 PROTO_FILES=$(ls .)
 
 for PROTO_FILE in $PROTO_FILES
 do
 echo "Compiling ${PROTO_FILE}..."
-grpc_tools_node_protoc --js_out=import_style=commonjs,binary:${CODEGEN_DIR} --grpc_out=${CODEGEN_DIR} --plugin=protoc-gen-grpc=`which grpc_tools_node_protoc_plugin` ${PROTO_FILE}
+grpc_tools_node_protoc --js_out=import_style=commonjs,binary:${WD}/${CODEGEN_DIR} \
+                       --grpc_out=${WD}/${CODEGEN_DIR} \
+                       --plugin=protoc-gen-grpc=`which grpc_tools_node_protoc_plugin` \
+                       ${PROTO_FILE}
 done
 
-cd ..
+cd ${WD}
